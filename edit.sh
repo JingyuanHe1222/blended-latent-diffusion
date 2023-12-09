4a9148d15897c4bab5422299e520ac923ebd2e95
Ubuntu <ubuntu@ip-172-31-18-74.ec2.internal>
2023-12-09 19:43:48 +0000

    result

--- edit.sh
+++ edit.sh
@@ -5,4 +5,4 @@
 # mask_object=""
 
 
-python scripts/text_editing_stable_diffusion.py --prompt "dog" --init_image "inputs/cat_2.jpg" --mask "inputs/mask_cat_2_cat.png" --output_path "outputs/cat_2_dog.png" --morph True
+python scripts/text_editing_stable_diffusion.py --prompt "cat" --init_image "inputs/woman_and_dog.jpg" --mask "inputs/mask_woman_and_dog_dog.png" --output_path "outputs/woman_and_dog_cat.jpg" --morph True
